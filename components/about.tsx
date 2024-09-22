047c85f3713313b4e78ca309483e6dfad6062253
Dasiel <dasieltorres99@gmail.com>
2024-09-22 04:06:32 -0400

    descargar cv 3

--- components/about.tsx
+++ components/about.tsx
@@ -62,12 +62,12 @@ export default function About() {
                             </DropdownMenuTrigger>
                             <DropdownMenuContent>
                                 <DropdownMenuItem>
-                                    <a href="/api/download?file=Dasiel Torres CV Resume (español).pdf" className="w-full">
+                                    <a href="/api/download?file=Dasiel%20Torres%20CV%20Resume%20(español).pdf" className="w-full">
                                         Español
                                     </a>
                                 </DropdownMenuItem>
                                 <DropdownMenuItem>
-                                    <a href="/api/download?file=Dasiel Torres CV Resume (inglés).pdf" className="w-full">
+                                    <a href="/api/download?file=Dasiel%20Torres%20CV%20Resume%20(inglés).pdf" className="w-full">
                                         English
                                     </a>
                                 </DropdownMenuItem>
